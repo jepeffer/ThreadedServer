8bf9ad5b2038126c9e3975328d136e3699bf6fbc
Julian Peffer <jepeffer@unomaha.edu>
2019-09-09 18:34:12 -0500

    Initial commit including base .net core files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ThreadedServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("LET ME INIT GIT");
+        }
+    }
+}
